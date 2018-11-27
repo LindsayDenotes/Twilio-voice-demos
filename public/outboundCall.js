eaf242df92887513faa5dadc26e2c35e32b0250c
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2018-11-27 12:14:18 -0500

    TwilioQuest: Voice https://www.twilio.com/quest/mission/2/objective/8

--- public/outboundCall.js
+++ public/outboundCall.js
@@ -5,7 +5,7 @@ const authToken = '3aae8d030a9fd79f310d0fe8c39ababf'; // to be put into environm
 // create Twilio rest client
 const client = require('twilio')(accountSid, authToken);
 
-// for creating outbound calls
+// for creating outbound calls. run node outboundCall.js from ~/dev/client-quickstart-js-master/public
 client.calls.create({
 	url: 'https://handler.twilio.com/twiml/EH6577a53b0a3dd80ea7ab4a9599f22b8a',
 	to: '+12023749435',
@@ -14,7 +14,8 @@ client.calls.create({
 	if(err){
 		console.log(err);
 	} else {
-		console.log(call.sid);
+		console.log(call.sid); // CAfdd55c7b752d4dae22b47df19c48da14
 	}
 });
+
 
